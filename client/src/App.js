02e97f942b8ba9162c56a36c86cde87dd1baed34
Warren Kozak <kozakw@carleton.edu>
2025-07-25 15:30:15 -0500

    Add simple editor view and create database container

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from './views/login';
 import SignUp from './views/signup';
 import Welcome from './views/welcome';
 import Test from './views/test';
+import Editor from './views/editor';
 
 const App = () => {
     return (
@@ -12,6 +13,8 @@ const App = () => {
                 <Route path="/" element={<Welcome/>} />
                 <Route path="/login" element={<Login/>} />
                 <Route path="/signup" element={<SignUp/>} />
+                <Route path="/editor" element={<Editor/>} />
+
                 <Route path="/test" element={<Test/>} />
             </Routes>
         </Router>
